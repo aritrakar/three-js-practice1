dec484eb9eb2db5e3c739d6fa6509ab11fbe5b50
Aritra Kar <aritrakar2002@gmail.com>
2022-07-02 09:03:58 -0400

    Trying to fix Tailwind CSS inclusion.

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import "./style.css";
 import * as THREE from "three";
 import * as dat from "dat.gui";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-// import { randFloatSpread } from "three/src/math/MathUtils";
 import gsap from "gsap";
 
 function jaggedPlane(planeMesh) {
